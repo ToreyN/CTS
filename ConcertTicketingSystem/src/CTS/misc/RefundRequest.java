ede7fe01306b1c152d89d35dffd6469c097259e1
ToreyN <torey_n@live.com>
2025-11-23 13:50:08 -0800

    Update RefundRequest.java

--- ConcertTicketingSystem/src/CTS/misc/RefundRequest.java
+++ ConcertTicketingSystem/src/CTS/misc/RefundRequest.java
@@ -1,5 +1,46 @@
 package CTS.misc;
 
 public class RefundRequest {
+    private int id;
+    private Order order;
+    private String reason;
+    private RefundStatus status = RefundStatus.PENDING;
+
+    public RefundRequest(int id, Order order, String reason) {
+        this.id = id;
+        this.order = order;
+        this.reason = reason;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public RefundStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RefundStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "RefundRequest{" +
+                "id=" + id +
+                ", orderId=" + order.getId() +
+                ", user=" + order.getUser().getName() +
+                ", status=" + status +
+                ", reason='" + reason + '\'' +
+                '}';
+    }
 
 }
