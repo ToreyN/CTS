5f560b00af69101e5016431f5e8acdf80c6f550e
ToreyN <torey_n@live.com>
2025-11-23 14:03:50 -0800

    Update Artist.java

--- ConcertTicketingSystem/src/CTS/event/Artist.java
+++ ConcertTicketingSystem/src/CTS/event/Artist.java
@@ -1,5 +1,43 @@
 package CTS.event;
 
 public class Artist {
+    private int artistId;
+    private String stageName;
+    private String genre;
+
+    public Artist(int artistId, String stageName, String genre) {
+        this.artistId = artistId;
+        this.stageName = stageName;
+        this.genre = genre;
+    }
+
+    public int getArtistId() {
+        return artistId;
+    }
+
+    public String getStageName() {
+        return stageName;
+    }
+
+    public void setStageName(String stageName) {
+        this.stageName = stageName;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "artistId=" + artistId +
+                ", stageName='" + stageName + '\'' +
+                ", genre='" + genre + '\'' +
+                '}';
+    }
 
 }
